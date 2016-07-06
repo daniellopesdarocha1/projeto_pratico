b678e2251c02328d5c85d7be8f3c25b3cca3dc84
daniellopesdarocha1 <daniellopesdarocha1@gmail.com>
2016-07-06 12:58:43 -0300

    adicionando imagens e insert

---
+++ bd/insert_de_imagem.sql
@@ -0,0 +1,5 @@
+INSERT INTO imagem(idimagem, imagemtitulo, imagemarquivo, noticia_idnoticia) 
+VALUES (1, 'teste', 'foto1.jpg', 1);
+
+INSERT INTO imagem(idimagem, imagemtitulo, imagemarquivo, noticia_idnoticia) 
+VALUES (2, 'teste', 'foto2.jpg', 1);
